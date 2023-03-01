c7d098ebc420f622a0dada0b38748c3863c1bf2a
dimasnihbos <90371402+dimasnihbos@users.noreply.github.com>
2023-03-01 09:53:37 +0700

    Add files via upload

---
+++ tugas-personal-profile/beranda.php
@@ -0,0 +1,98 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Tentang Kami</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="TentangKami.css">
+  </head>
+  <body>
+    <div></div>
+    <nav class="navbar navbar-expand-lg bg-primary fixed-top" >
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Identitas Sayaaaaaaaa</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0 " style="margin-left: 800px;">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Tentang Zafana</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Mengapa Zafana</a>
+          </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Tentang Kami</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<div>
+    <img src="GAMBARZAFANA3.PNG" alt="" width="100%">
+</div>
+<div class="container">
+<br>
+<br>
+<div>
+  <center> <h1>Zafana adalah platform belanja online terdepan di Asia Tenggara dan Cikampek.</h1></center>
+</div>
+<br>
+<div>
+    <center><p>Diluncurkan tahun 2022, Zafana merupakan sebuah platform yang disesuaikan untuk tiap wilayah dan menyediakan pengalaman berbelanja online yang mudah, aman, dan cepat bagi pelanggan melalui dukungan pembayaran dan logistik yang kuat.
+        Kami percaya bahwa kegiatan belanja online harus terjangkau, mudah, dan menyenangkan. Ini adalah visi yang ingin Zafana berikan melalui platform kami, setiap harinya.</p></center>
+</div>
+<br>
+<div class="d-grid gap-2 col-6 mx-auto">
+    <button class="btn btn-primary" type="button">Jelajahi Zafana</button>
+ </div>
+<br>
+<br>
+<br>
+<br>
+<br>
+ <div class="container text-center">
+  <div class="row">
+    <div class="col">
+      <h1>Tujuan kami</h1>
+    <p>Kami percaya pada kekuatan transformatif dari teknologi dan ingin mengubah dunia menjadi lebih baik dengan menyediakan platform untuk menghubungkan pembeli dan penjual dalam satu komunitas.</p> 
+    </div>
+    <div class="col">
+      <h1>Posisi Kami</h1>
+    <p> Untuk pengguna Internet di seluruh wilayah, Shopee menawarkan pengalaman belanja online komprehensif, dari berbagai pilihan produk sampai ke sebuah komunitas sosial untuk bereksplorasi, dan layanan untuk selalu memenuhi kebutuhan konsumen tanpa hambatan.</p>
+    </div>
+  </div>
+  <br>
+  <br>
+  <br>
+  <br>
+<div class="kepribadian">
+  <h1>Kepribadian Kami</h1> 
+  <p> Untuk menggambarkan siapa kami - bagaimana kami berbicara, bertindak, dan bereaksi terhadap situasi tertentu - pada dasarnya, kami Simpel, Bahagia, dan Bersama-sama. Nilai-nilai utama ini selalu terlihat dalam setiap langkah perjalanan Shopee.</p>
+  <br>
+  <div class="container text-center">
+    <div class="row">
+      <div class="col">
+       <h1> Simpel</h1>
+      Kami percaya akan kesederhanaan dan integritas; memastikan kehidupan yang jujur, rendah hati, dan apa adanya.
+      </div>
+      <div class="col">
+        <h1>Bersama-sama</h1> 
+        Kami menikmati menghabiskan waktu bersama dengan teman-teman dan keluarga sekaligus berbelanja online, serta melakukan hal-hal yang kami sukai sebagai satu kesatuan.
+      </div>
+      <div class="col">
+       <h1> Bahagia</h1>
+      Kami ramah, menyenangkan dan energetik, serta menyebarkan sukacita kepada semua orang yang kami temui.
+      </div>
+    </div>
+  </div>
+
+</div>
+
+</div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+  </body>
+</html>
